d6e8a397334d58b9ea09eb0c85970450527e5661
sriramtalasila <sriramprasadtalasila@gmail.com>
2019-08-05 00:31:22 +0530

    two pages

--- js/productinfo.js
+++ js/productinfo.js
@@ -1,3 +1,45 @@
 $(document).ready(()=>{
-	console.log(localStorage.getItem("ProductId"));
+
+	var firebaseConfig = {
+		apiKey: "AIzaSyAY9D0z96KVPjIedXhRfYSFpq9uhTSS2fg",
+		authDomain: "inventory-745b2.firebaseapp.com",
+		databaseURL: "https://inventory-745b2.firebaseio.com",
+		projectId: "inventory-745b2",
+		storageBucket: "",
+		messagingSenderId: "925122077794",
+		appId: "1:925122077794:web:59908a6cddd42fe0"
+	};
+	// Initialize Firebase
+	firebase.initializeApp(firebaseConfig);
+
+	var database = firebase.database();
+	var categoryID = localStorage.getItem("ProductId");
+	
+	database.ref("Category/"+categoryID+"/products/").on('value',(snapshot)=>{
+		var data = snapshot.val();
+		console.log(data);
+		$("tbody").empty();
+		var i=1;
+		for(var key in data){
+			var c = '<tr><td>'+(i++)+'</td><td><h5>'+data[key].Product_Name+'</h5><p>'+data[key].description+'</p></td></tr>';
+			$(c).appendTo("tbody")
+			console.log(data[key])
+		}
+	})
+
+
+	$("#add").click(() => {
+		var name = $("#name").val();
+		var description = $("#description").val();
+		database.ref("Category/"+categoryID+"/products/").push().set({
+			Product_Name: name,
+			description: description,
+
+		}, () => {
+			alert("new Product added succesfully");
+			$("#name").val("");
+			$("#description").val("");
+			$("#myModal").modal('toggle');
+		});
+	})
 })
--- js/products.js
+++ js/products.js
@@ -1,6 +1,60 @@
-$(document).ready(()=>{
-	
-	function navigate(){
-		console.log("hello");
+
+$(document).ready(() => {
+
+	var firebaseConfig = {
+		apiKey: "AIzaSyAY9D0z96KVPjIedXhRfYSFpq9uhTSS2fg",
+		authDomain: "inventory-745b2.firebaseapp.com",
+		databaseURL: "https://inventory-745b2.firebaseio.com",
+		projectId: "inventory-745b2",
+		storageBucket: "",
+		messagingSenderId: "925122077794",
+		appId: "1:925122077794:web:59908a6cddd42fe0"
+	};
+	// Initialize Firebase
+	firebase.initializeApp(firebaseConfig);
+
+	var database = firebase.database();
+	loadPage();
+
+	writeUserData("ss", "name", "emmnda", "dsds");
+	function writeUserData(userId, name, email, imageUrl) {
+		database.ref('users/' + userId).set({
+			username: name,
+			email: email,
+			profile_picture: imageUrl
+		});
 	}
+
+
+	$("#add").click(() => {
+		var name = $("#name").val();
+		var description = $("#description").val();
+		database.ref('Category/').push().set({
+			Category_Name: name,
+			description: description,
+
+		}, () => {
+			alert("new Product added succesfully");
+			$("#name").val("");
+			$("#description").val("");
+			$("#myModal").modal('toggle');
+		});
+	})
+
+	function loadPage() {
+		database.ref('Category/').on('value', function (snapshot) {
+			var data = snapshot.val();
+			var i = 1;
+			$("tbody").empty();
+			for (var key in data) {
+				var c = '<tr><td>' + (i++) + '</td><td><a href="./productinfo.html	" onclick="navigate()"><h5 data-id ="' + key + '">' + data[key].Category_Name + '</h5></a><p>' + data[key].description
+					+ '</p></td></tr>'
+				$(c).appendTo("tbody");
+			}
+		});
+	}
+
+	$("h5").click(() => {
+
+	})
 })
