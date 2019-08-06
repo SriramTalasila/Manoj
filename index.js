0fea5652277d2be096e42aabc154d0b321f28532
Talasila <sri-ram-prasad.talasila@capgemini.com>
2019-08-06 11:04:01 +0530

    added node server modifications

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
-express().use(express.static(path.join(__dirname, 'public')))
-express().listen(PORT, () => {
+var app = express()
+app.use('/',express.static(path.join(__dirname, 'public')))
+app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 })
