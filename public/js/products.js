88829f5baa95787cdf870dc89c855c02b891fb1b
Talasila <sri-ram-prasad.talasila@capgemini.com>
2019-08-08 14:47:08 +0530

    add back and home

--- public/js/products.js
+++ public/js/products.js
@@ -14,7 +14,7 @@ $(document).ready(() => {
 	firebase.initializeApp(firebaseConfig);
 
 	var database = firebase.database();
-	loadPage();
+
 
 	$("#add").click(() => {
 		var name = $("#name").val();
@@ -31,25 +31,37 @@ $(document).ready(() => {
 		});
 	})
 
-	function loadPage() {
-		database.ref('Category/').on('value', function (snapshot) {
-			var data = snapshot.val();
-			var i = 1;
-			var dataArray = [];
-			$("tbody").empty();
-			for (var key in data) {
-				var obj = {
-					"key": key
-				};
-				dataArray[i] = key;
-				var c = '<tr><td>' + (i) + '</td><td><h5 onclick="navigate(' + i + ')" data-id ="' + key + '">' + data[key].Category_Name + '</h5><p>' + data[key].description
-					+ '</p></td></tr>'
-				$(c).appendTo("tbody");
-				i++;
+
+	database.ref('Category/').on('value', function (snapshot) {
+		var data = snapshot.val();
+		var i = 1;
+		var dataArray = [];
+		$("tbody").empty();
+		for (var key in data) {
+			var obj = {
+				"key": key
+			};
+			dataArray[i] = key;
+			var c = '<tr><td>' + (i) + '</td><td><h5 onclick="navigate(' + i + ')" data-id ="' + key + '">' + data[key].Category_Name + '</h5><p>' + data[key].description
+				+ '</p></td><td><i class="fa fa-times-circle" style="font-size:36px;color:red"></i></td></tr>'
+			$(c).appendTo("tbody");
+			i++;
+		}
+		localStorage.setItem("dataArray", dataArray);
+		$('tbody>tr>td>i').click((e) => {
+			var catId = $(e.target).closest('tr').find('td>h5')[0].getAttribute("data-id");
+			var promt = prompt("Are you sure(yes/no)")
+			if (promt == "yes" || promt == "YES" || promt == "Yes") {
+				database.ref('Category/' + catId + '/').remove().then(function () {
+					alert("Remove succeeded.")
+				})
+					.catch(function (error) {
+						alert("Remove failed: " + error.message)
+					});
 			}
-			localStorage.setItem("dataArray", dataArray);
-			//console.log(dataArray);
-		});
-	}
+		})
+		//console.log(dataArray);
+	});
+
 })
 
